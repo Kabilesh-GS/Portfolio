43b8e6e9a6cc27a53dfc7e91ed432fdc0b412f71
Kabilesh-GS <kabileshgs@gamil.com>
2023-11-19 15:17:03 +0530

    minor changes

--- script.js
+++ script.js
@@ -9,4 +9,16 @@ function appear1(){
       y.style.display = "inline-block";
     }
 
-  };
+};
+function appear2(){
+    var x = document.getElementById("iteam2title");
+    var y = document.getElementById("iteam02");
+    if (x.style.display === "none") {
+      x.style.display = "inline-block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "inline-block";
+    }
+
+};
