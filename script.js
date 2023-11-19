6ac3bddc49e74bc7f13be126f248b3d68f1fca8b
Kabilesh-GS <kabileshgs@gamil.com>
2023-11-19 14:53:55 +0530

    added timeline and some js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function appear1(){
+    var x = document.getElementById("iteam1title");
+    var y = document.getElementById("iteam01");
+    if (x.style.display === "none") {
+      x.style.display = "inline-block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "inline-block";
+    }
 
+  };
