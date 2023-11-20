fb97a8a677d72beab55f3068695f445222222006
Kabilesh-GS <kabileshgs@gamil.com>
2023-11-20 21:04:39 +0530

    added content

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ function appear2(){
     }
 
 };
+function appear3(){
+  var x = document.getElementById("iteam3title");
+  var y = document.getElementById("iteam03");
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "inline-block";
+  }
+
+};
