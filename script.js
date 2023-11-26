f730e3004b01db40eb7708d4f0b9a5ecb2ded44f
Kabilesh-GS <kabileshgs@gamil.com>
2023-11-26 14:49:17 +0530

    added scroll to top button

--- script.js
+++ script.js
@@ -75,3 +75,21 @@ function appear7() {
     y.style.display = "inline-block";
   }
 }
+let mybutton = document.getElementById("topbutton");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function Scrolltotop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
