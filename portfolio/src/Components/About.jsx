0949ab64b6f53e2326370d27e1aa11055b1dfb33
Kabilesh-GS <kabileshgs@gmail.com>
2024-05-05 09:47:14 +0530

    working on tech stack.

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Aboutme from './Aboutme';
+import Techstack from './Techstack';
 
 function About() {
   return (
-    <div style={{display: 'flex',justifyContent: 'center',alignItems: 'center',marginTop: '22px'}}>
+    <div style={{display: 'flex',justifyContent: 'center',flexDirection: 'column', gap: '20px',alignItems: 'center',marginTop: '22px'}}>
+      <div>
         <Aboutme />
+      </div>
+      <div style={{display:'flex',gap: '20px'}}>
+        <Techstack />
+        <Techstack />
+      </div>
     </div>
   )
 }
 
-export default About
+export default About;
---
+++ portfolio/src/Components/Techstack.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tech from './Techstack.module.css';
+
+function Techstack() {
+  return (
+    <div className={Tech.container}>
+        <h1>Tech</h1>
+    </div>
+  )
+}
+
+export default Techstack;
