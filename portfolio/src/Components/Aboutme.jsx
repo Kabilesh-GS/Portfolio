4ce79f5c59dfae2d0baea404406aaab43211bc42
Kabilesh-GS <kabileshgs@gmail.com>
2024-05-06 17:35:07 +0530

    Edited About.

--- portfolio/src/Components/Aboutme.jsx
+++ portfolio/src/Components/Aboutme.jsx
@@ -6,8 +6,8 @@ function Aboutme() {
     <div>
         <div className={`${Aboutmestyle.container}`}>
             <p className={`${Aboutmestyle.heyeveryone}`}>Hey Everyone ! 👋</p>
-            <p className={`${Aboutmestyle.paragraph}`}>I’m Kabilesh an avid learner with a creative mindset. I’m in pursuit of B.Tech in Information Technology in Coimbatore aiming to elevate my programming proficiency both creatively and critically. </p>
-            <p className={`${Aboutmestyle.paragraph}`}>I always wondered how websites are designed and made. This curiosity made me start learning web technologies with Spotknack. Currently an Intern at Spotknack. I’ve done multiple projects while learning and also as an intern.</p>
+            <p className={`${Aboutmestyle.paragraph}`}>I’m Kabilesh an avid learner with a creative mindset. I’m in pursuit of B.Tech in Information Technology in Coimbatore aiming to elevate my programming proficiency both creatively and critically. I always wondered how websites are designed and made. This curiosity made me start learning web technologies with Spotknack. Currently an Intern at Spotknack. I’ve done multiple projects while learning and also as an intern.</p>
+            <p className={`${Aboutmestyle.paragraph}`}>Beyond academics I like to explore new and adventurous thing and places. I'm a big fan of both Cricket and football, so playing them and watching movies and series are my hobbies.</p>
         </div>
     </div>
   )
