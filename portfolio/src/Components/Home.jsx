4f4fd32d915e8b3f5ea9d0c5f7176af31b143c43
Kabilesh-GS <kabileshgs@gmail.com>
2024-05-22 22:58:16 +0530

    Minor Changes

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Homestyle from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -8,7 +9,7 @@ function Home() {
             <p className={`${Homestyle.robotoflex} ${Homestyle.semibold} ${Homestyle.font40px}`}>Hey, I’m Kabilesh a passionate<br />Frontend developer.</p>
             <div className={`${Homestyle.flex2}`}>
               <p className={`${Homestyle.questrial} ${Homestyle.margin16px} ${Homestyle.font25px} ${Homestyle.lineheight}`}>I’m currently working as an Intern <br />at Spotknack. </p>
-              <a href='https://connect-with-kabi.netlify.app/' target='_blank'><button onclick="window.location.href = 'https://connect-with-kabi.netlify.app/';" className={`${Homestyle.button} ${Homestyle.robotoflex}`}>Contact Me</button></a>
+              <button className={`${Homestyle.button} ${Homestyle.robotoflex}`}><Link style={{textDecoration:'none', color: 'white'}} to='#Contact'>Let's Talk !</Link></button>
             </div>
         </div>
     </div>
