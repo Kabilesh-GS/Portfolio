b0a08902614ecc1e97809db2804540086cdcf50d
Kabilesh-GS <kabileshgs@gmail.com>
2024-06-14 13:37:23 +0530

    Removed unwanted imports

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Prostyle from './Project.module.css';
 import Oneplusvideo from '../assets/1Plus.mp4'
 import Projectscontainer from './Projectscontainer';
-import Footer from '../Components/Footer';
 
 function Projects() {
   return (
