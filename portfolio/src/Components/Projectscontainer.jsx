45a964afb178fa3ff3735806d8c7d22290749bf9
Kabilesh-GS <kabileshgs@gmail.com>
2024-06-15 13:05:32 +0530

    Completed Projects page.

--- portfolio/src/Components/Projectscontainer.jsx
+++ portfolio/src/Components/Projectscontainer.jsx
@@ -6,6 +6,8 @@ function Projectscontainer(props) {
     <div className={Projectstyle.container}>
       <video autoPlay={true} muted={true} loop={true} className={Projectstyle.widthvideo} src={props.videosrc}></video>
       <p className={Projectstyle.styleheading}>{props.contentheading}</p>
+      <p className={Projectstyle.projectDes}>{props.content}</p>
+      <p className={Projectstyle.link}><a target='_blank' href={props.link}>Check it out !  </a></p>
     </div>
   )
 }
