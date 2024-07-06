4104512dee39d2b1fca8f675c008126b918a52d6
Kabilesh-GS <kabileshgs@gmail.com>
2024-07-06 13:12:46 +0530

    Changes

--- portfolio/src/Components/Blog.jsx
+++ portfolio/src/Components/Blog.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import BlogStyle from './Blog.module.css'
+import BlogStyle from './Blog.module.css';
+import image from '../assets/MediumImage.png'
 
 function Blog() {
   return (
     <div className={`${BlogStyle.Container}`}>
-      home
+      <a href='https://medium.com/@kabileshgs/tale-of-2-cities-in-2-days-bengaluru-and-chikkamagaluru-3618794b8bce' target='_blank'><img style={{width: '400px'}} src={image} /></a>
     </div>
   )
 }
