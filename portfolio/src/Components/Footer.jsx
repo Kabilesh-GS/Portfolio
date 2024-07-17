4c909c90b51b350a798bff7bf179efc8a6a49c35
Kabilesh-GS <kabileshgs@gmail.com>
2024-07-17 17:12:24 +0530

    Color changes

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={Footerstyle.footerDiv}>
         <p className={Footerstyle.footer}>Made With 💖 By Me!</p>
-        <p className={Footerstyle.lastupdated}>Last updated 12-07-2024</p>
+        <p className={Footerstyle.lastupdated}>Last updated 17-07-2024</p>
     </footer>
   )
 }
