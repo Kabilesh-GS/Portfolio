bc74183bec88246f379a315a40202b64faa1170c
Kabilesh-GS <kabileshgs@gmail.com>
2024-07-18 12:26:05 +0530

    minor changes

--- portfolio/src/Components/Aboutexperience.jsx
+++ portfolio/src/Components/Aboutexperience.jsx
@@ -8,7 +8,7 @@ function Aboutexperience() {
       <img className={Aboutexp.image} src={Spotknack} />
       <div className={Aboutexp.container}>
         <p className={`${Aboutexp.role} ${Aboutexp.marginleftright}`}>Frontend Developer</p>
-        <p className={`${Aboutexp.time} ${Aboutexp.marginleftright}`}>5 months - February(2024) - Present</p>
+        <p className={`${Aboutexp.time} ${Aboutexp.marginleftright}`}>6 months - February(2024) - Present</p>
         <p className={`${Aboutexp.des} ${Aboutexp.marginleftright}`}>Currently working as a Frontend Developer intern in Spotknack to create React web application to enhance their rewards platform. We have used react packages like ChartJS to improve the user interactivity of the application. The application is integrated with the back end with the help of Axios.</p>
       </div>
     </div>
