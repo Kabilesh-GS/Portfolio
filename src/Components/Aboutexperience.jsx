2757100d87beebf1114fbe48fb683ffbe31bd424
Kabilesh-GS <kabileshgs@gmail.com>
2024-10-30 14:06:21 +0530

    Updated experience

--- src/Components/Aboutexperience.jsx
+++ src/Components/Aboutexperience.jsx
@@ -9,7 +9,7 @@ function Aboutexperience() {
       <div className={Aboutexp.container}>
         <p className={`${Aboutexp.role} ${Aboutexp.marginleftright}`}>Frontend Developer</p>
         <p className={`${Aboutexp.time} ${Aboutexp.marginleftright}`}>9 months - February(2024) - October(2024)</p>
-        <p className={`${Aboutexp.des} ${Aboutexp.marginleftright}`}>Currently working as a Frontend Developer intern in Spotknack to create React web application to enhance their rewards platform. We have used react packages like ChartJS to improve the user interactivity of the application. The application is integrated with the back end with the help of Axios.</p>
+        <p className={`${Aboutexp.des} ${Aboutexp.marginleftright}`}>Worked as a Frontend Developer intern in Spotknack to create React web application to enhance their rewards platform. We used react packages like ChartJS to improve the user interactivity of the application. The application is integrated with the back end with the help of Axios.</p>
       </div>
     </div>
   )
