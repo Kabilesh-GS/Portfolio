12d2771ccc3d179efec0528923b9c3900fa52534
Kabilesh-GS <kabileshgs@gmail.com>
2024-11-16 13:22:54 +0530

    Updates.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ import Footerstyle from './Footer.module.css';
 function Footer() {
   return (
     <footer className={Footerstyle.footerDiv}>
-        <p className={Footerstyle.footer}>Made With ❤️ By Me!</p>
-        <p className={Footerstyle.lastupdated}>Last updated 27-10-2024</p>
+        <p className={Footerstyle.footer}>Made With 🤍 By Me!</p>
+        <p className={Footerstyle.lastupdated}>Last updated 16-11-2024</p>
     </footer>
   )
 }
