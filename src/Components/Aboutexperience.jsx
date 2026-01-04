de0aa89a7f94753f08f4e0c3274cd58f28cac8bc
Kabilesh-GS <kabileshgs@gmail.com>
2026-01-04 12:01:58 +0530

    added meta tags

--- src/Components/Aboutexperience.jsx
+++ src/Components/Aboutexperience.jsx
@@ -8,7 +8,7 @@ function Aboutexperience() {
       <img className={Aboutexp.image} src={Whizlabs} />
       <div className={Aboutexp.container}>
         <p className={`${Aboutexp.role} ${Aboutexp.marginleftright}`}>Product Trainee</p>
-        <p className={`${Aboutexp.time} ${Aboutexp.marginleftright}`}>December(2025) - </p>
+        <p className={`${Aboutexp.time} ${Aboutexp.marginleftright}`}>December(2025) - Present</p>
         <p className={`${Aboutexp.des} ${Aboutexp.marginleftright}`}>I joined in Whizlabs as a Product Trainee, where I’m gaining exposure to how technology, users, and business come together in real-world products.</p>
       </div>
     </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={Footerstyle.footerDiv}>
         <p className={Footerstyle.footer}>Made With ♥️ By Me!</p>
-        <p className={Footerstyle.lastupdated}>Last updated 30-12-2025</p>
+        <p className={Footerstyle.lastupdated}>Last updated 04-01-2026</p>
     </footer>
   )
 }
