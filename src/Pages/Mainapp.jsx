3d2c8c3c945bface7ecbc6f70905fa3ce7865571
Kabilesh-GS <kabileshgs@gmail.com>
2026-03-25 11:25:44 +0530

    removed old components and installed tailwind

--- src/Pages/Mainapp.jsx
+++ src/Pages/Mainapp.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
-import EntireHome from './EntireHome';
-import About from './About';
-import Experience from './Experience';
-import Projects from './Projects';
-import Contact from './Contact';
+
 
 function Mainapp() {
   return (
     <BrowserRouter>
-        <Navbar />
+        <h1 className=''>hello</h1>
         <Routes>
-            <Route path="/" element={<EntireHome />}/>
-            <Route path="about" element={<About />}/>
-            <Route path="experience" element={<Experience />}/>
-            <Route path="projects" element={<Projects />}/>
-            <Route path='contact' element={<Contact />} />
+          <Route path='/health' element={<p>All good</p>}/>
         </Routes>
     </BrowserRouter>
   )
