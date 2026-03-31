3b5431dd19002e61968fd4357778191e499d77ae
Kabilesh-GS <kabileshgs@gmail.com>
2026-03-31 22:30:20 +0530

    added home image

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,18 +6,18 @@ function Navbar({icon}) {
   const path = useLocation();
 
   return (
-    <div className="fixed">
+    <div className="fixed z-100">
       <div className={`${hover ? "bottom-12 md:bottom-15" : "-bottom-9"} fixed left-1/2 -translate-x-1/2 flex gap-10 font-[Sora] text-white text-lg transition-all duration-200`} >
-        <Link to="/" className={`${path.pathname == "/" ? "border border-white/50" : ""} absolute md:relative -left-5 text-sm md:text-lg translate-y-10 hover:scale-110 transition-all duration-200 bg-gradient-to-l from-white/25 via-white/10 to-transparent backdrop-blur-md px-4 rounded-full`}>
+        <Link to="/" className={`${path.pathname == "/" ? "border border-white/50" : ""} absolute md:relative -left-5 text-sm md:text-lg translate-y-10 hover:scale-110 transition-all bg-white/5 backdrop-blur-md border justify-center border-white/10 shadow-lg shadow-black/30 duration-200 px-4 rounded-full`}>
           Home
         </Link>
-        <Link to="/projects" className={`${path.pathname == "/projects" ? "border border-white/50" : ""} translate-y-1 text-sm md:text-lg hover:scale-110 transition-all duration-200 bg-gradient-to-t from-white/25 via-white/10 to-transparent backdrop-blur-md px-4 rounded-full`}>
+        <Link to="/projects" className={`${path.pathname == "/projects" ? "border border-white/50" : ""} translate-y-1 text-sm md:text-lg hover:scale-110 transition-all duration-200 bg-white/5 backdrop-blur-md border justify-center border-white/10 shadow-lg shadow-black/30 px-4 rounded-full`}>
           Projects
         </Link>
-        <Link to="/experience" className={`${path.pathname == "/experience" ? "border border-white/50" : ""} translate-y-1 text-sm md:text-lg hover:scale-110 transition-all duration-200 bg-gradient-to-t from-white/25 via-white/10 to-transparent backdrop-blur-md px-4 rounded-full`}>
+        <Link to="/experience" className={`${path.pathname == "/experience" ? "border border-white/50" : ""} translate-y-1 text-sm md:text-lg hover:scale-110 transition-all duration-200 bg-white/5 backdrop-blur-md border justify-center border-white/10 shadow-lg shadow-black/30 px-4 rounded-full`}>
           Experience
         </Link>
-        <Link to="/contact" className={`${path.pathname == "/contact" ? "border border-white/50" : ""} absolute -right-5 md:relative text-sm md:text-lg translate-y-10 hover:scale-110 transition-all duration-200 bg-gradient-to-r from-white/25 via-white/10 to-transparent backdrop-blur-md px-4 rounded-full`}>
+        <Link to="/contact" className={`${path.pathname == "/contact" ? "border border-white/50" : ""} absolute -right-5 md:relative text-sm md:text-lg translate-y-10 hover:scale-110 transition-all duration-200 bg-white/5 backdrop-blur-md border justify-center border-white/10 shadow-lg shadow-black/30 px-4 rounded-full`}>
           Contact
         </Link>
       </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,41 +3,69 @@ import REACT from '../../assets/react.svg'
 import NEST from '../../assets/NestJS.svg'
 import POSTGRES from '../../assets/Postgresql.png'
 import TAILWIND from '../../assets/tailwind-css.svg'
+import ME from '../../assets/kabi 3.png'
 
 function Home() {
   return (
     <div className="text-white">
       <div className="backdrop-blur-xl rounded-2xl mt-8 mx-8 px-15 py-5 bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center">
-        <div>
-          <h1 className="font-[Sora] text-3xl md:text-5xl font-semibold tracking-tight">Hey! Kabilesh here,</h1>
-          <p className="font-[Sora] text-lg text-white/70 mt-1">And I build things that live on the internet.</p>
+        <div className="font-[Sora]">
+          <h1 className="text-3xl md:text-5xl font-semibold tracking-tight">Hey! This is Kabilesh,</h1>
+          <p className="text-lg text-white/70 mt-1">And I build things that live on the internet.</p>
         </div>
         <div>
-          <Link to='/contact' className="px-7 py-3 hover:text-lg transition-all duration-200 rounded-full bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl shadow-sm shadow-black/30">Talk with me?</Link>
+          <Link to='/contact' className="px-7 font-[Sora] py-3 hover:text-lg transition-all duration-200 rounded-full bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl shadow-sm shadow-black/30">Talk with me?</Link>
         </div>
       </div>
-      <div className="">
+      <div className="flex items-start">
 
-        <div className="flex flex-col w-[50%] justify-center ">
+        <div className="flex flex-col w-[65%] justify-center ">
 
-          <div className="flex mx-8 bg-white/5 backdrop-blur-md border justify-center border-white/10 rounded-2xl shadow-lg shadow-black/30 mt-5 py-5 px-15 gap-10 rounded-2xl">
-            <img src={REACT} className="w-25"/>
-            <img src={TAILWIND} className="w-25"/>
-            <img src={NEST} className="w-25"/>
-            <img src={POSTGRES} className="w-25"/>
+          <div className="flex mx-8 bg-white/5 backdrop-blur-md border justify-center border-white/10 rounded-2xl shadow-lg shadow-black/30 mt-5 py-5 px-15 gap-10">
+            <div className="relative group/icon flex flex-col items-center">
+              <img src={REACT} className="w-25 transition-all duration-300 group-hover/icon:scale-110"/>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover/icon:opacity-100 group-hover/icon:translate-y-2 transition-all duration-300 text-xs px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30 whitespace-nowrap">React</p>
+            </div>
+            <div className="relative group/icon flex flex-col items-center">
+              <img src={TAILWIND} className="w-25 transition-all duration-300 group-hover/icon:scale-110"/>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover/icon:opacity-100 group-hover/icon:translate-y-2 transition-all duration-300 text-xs px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30 whitespace-nowrap">Tailwind </p>
+            </div>
+            <div className="relative group/icon flex flex-col items-center">
+              <img src={NEST} className="w-25 transition-all duration-300 group-hover/icon:scale-110"/>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover/icon:opacity-100 group-hover/icon:translate-y-2 transition-all duration-300 text-xs px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30 whitespace-nowrap">NestJS</p>
+            </div>
+            <div className="relative group/icon flex flex-col items-center">
+              <img src={POSTGRES} className="w-25 transition-all duration-300 group-hover/icon:scale-110"/>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover/icon:opacity-100 group-hover/icon:translate-y-2 transition-all duration-300 text-xs px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30 whitespace-nowrap">PostgreSQL</p>
+            </div>
+
           </div>
 
-          <div className="flex mx-8 bg-white/5 backdrop-blur-md border justify-center border-white/10 rounded-2xl shadow-lg shadow-black/30 mt-5 py-5 px-15 gap-10 rounded-2xl">
-            <p className="text-[60px]">🎬</p>
-            <p className="text-[60px]">🏏</p>
-            <p className="text-[60px]">🎨</p>
-            <p className="text-[60px]">⚽️</p>
+          <div className="flex cursor-default mx-8 bg-white/5 backdrop-blur-md border justify-center border-white/10 rounded-2xl shadow-lg shadow-black/30 mt-5 py-5 px-15 gap-18">
+            <div className="relative group flex flex-col items-center">
+              <p className="text-[70px] transition-all duration-300 group-hover:scale-110">🎬</p>
+              <p className="absolute bottom-0 text-center translate-y-full mt-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-300 text-[10px] px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30">Movies/Series</p>
+            </div>
+            <div className="relative group flex flex-col items-center">
+              <p className="text-[70px] transition-all duration-300 group-hover:scale-110">🏏</p>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-300 text-[10px] px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30">Cricket</p>
+            </div>
+            <div className="relative group flex flex-col items-center">
+              <p className="text-[70px] transition-all duration-300 group-hover:scale-110">🎨</p>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-300 text-[10px] px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30">Drawing</p>
+            </div>
+            <div className="relative group flex flex-col items-center">
+              <p className="text-[70px] transition-all duration-300 group-hover:scale-110">⚽</p>
+              <p className="absolute bottom-0 translate-y-full mt-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-300 text-[10px] px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30">Football</p>
+            </div>
+
           </div>
 
         </div>
 
-        <div>
-
+        <div className="group relative mt-5">
+          <img className="shadow-sm shadow-black/30 rounded-2xl" src={ME}/>
+          <p className="absolute bottom-5 left-[43%] translate-y-full mt-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-300 text-md px-3 py-1 bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-sm shadow-black/30">Yep! That's me</p>
         </div>
 
       </div>
