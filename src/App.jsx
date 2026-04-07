61186382698eeb39b588f930054202feb628d0bf
Kabilesh-GS <kabileshgs@gmail.com>
2026-04-07 12:33:46 +0530

    updated Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css';
 import Mainapp from './Pages/Mainapp';
 import { navContext } from './Context/NavIconContext';
+import { FooterContext } from './Context/FooterContext';
 
 function App() {
 
   let navbarIcon = ['🤯','🤩','😍','😎']
+  let footerIcon = ['❤️', '♥️' , '🫶' , '💛' , '💜']
   const navbarLen = navbarIcon.length;
-  const random = Math.floor(Math.random() * navbarLen);
+  const footerLen = footerIcon.length;
+  const random1 = Math.floor(Math.random() * navbarLen);
+  const random2 = Math.floor(Math.random() * footerLen);
+
   const navEmoji = {
-    emoji : navbarIcon[random]
+    emoji : navbarIcon[random1]
+  }
+  const footerEmoji = {
+    emoji : footerIcon[random2]
   }
 
   return(
-    <navContext.Provider value={navEmoji}>
-      <Mainapp/>
-    </navContext.Provider>
+    <FooterContext.Provider value={footerEmoji}>
+      <navContext.Provider value={navEmoji}>
+        <Mainapp/>
+      </navContext.Provider>
+    </FooterContext.Provider>
   );
 }
 
